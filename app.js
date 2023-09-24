0fc791235183ecc9a429ee5737355359cb78a0a4
Pablo <pablomiranda.cl@gmail.com>
2023-09-24 15:22:04 -0300

    Mayor fixes and upgrade to Node 18

--- app.js
+++ app.js
@@ -2,29 +2,20 @@ var config  = require('./config/environ');
 var express = require('express');
 
 var app       = express();
-//var multer    = require('multer');
 var constants = require('constants');
 var constant = require('./config/constants');
-
-//var passport = require('passport');
-//var flash = require('connect-flash');
 var path = require('path');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-//var dateFormat = require('dateformat');
-
 var serveIndex = require('serve-index');
 var now = new Date();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded(
        {extended: true}));
-
-//require('./config/passport')(passport);
-// pass passport for configuration
 
 //set up our express application
 app.use(morgan('dev')); 
@@ -42,40 +33,24 @@ app.use(express.static(path.join
 	
 app.set('views', path.join
 	(__dirname, 'app/views'));
+	
 app.set('view engine', 'ejs');
 
 app.use('/css',serveIndex
 	(__dirname + '/public/css'));
+	
 app.use('/js',serveIndex
 	(__dirname + '/public/js'));
 
-// set up ejs for templating
-
 console.log(__dirname);
 
-//app.use(session({
-//    secret: 'I Love India...',
-//    resave: true,
-//    saveUninitialized: true
-//}));
-
-//app.use(passport.initialize());
-//app.use(passport.session());
-
-// persistent login sessions
-//app.use(flash());
-
-// use connect-flash for flash 
-// messages stored in session
-
-//
 var nocache = require('nocache')
 app.use(nocache());
 
 // routes
 var routes=require('./config/routes.js');
 app.use(routes);
-// load our routes
+
 
 app.listen(config.port,function()
 {console.log(config.port)});
